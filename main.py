a9bb2546e9a1f7ef225ed0614eba3723fc105339
MrJonRoberts <jroberts@tas.qld.edu.au>
2021-04-21 08:21:49 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,35 +7,35 @@
 jack = { "name":"Jack Frost",
 		"assignment" : [80, 50, 40, 20],
 		"test" : [75, 75],
-		"lab" : [78.20, 77.20]
+		"class" : [78.20, 77.20]
 	}
 		
 # 2. James's dictionary
 james = { "name":"James Potter",
 		"assignment" : [82, 56, 44, 30],
 		"test" : [80, 80],
-		"lab" : [67.90, 78.72]
+		"class" : [67.90, 78.72]
 		}
 
 # 3. Dylan's dictionary
 dylan = { "name" : "Dylan Rhodes",
 		"assignment" : [77, 82, 23, 39],
 		"test" : [78, 77],
-		"lab" : [80, 80]
+		"class" : [80, 80]
 		}
 		
 # 4. Jessica's dictionary
 jess = { "name" : "Jessica Stone",
 		"assignment" : [67, 55, 77, 21],
 		"test" : [40, 50],
-		"lab" : [69, 44.56]
+		"class" : [69, 44.56]
 	}
 		
 # 5. Tom's dictionary
 tom = { "name" : "Tom Hanks",
 		"assignment" : [29, 89, 60, 56],
 		"test" : [65, 56],
-		"lab" : [50, 40.6]
+		"class" : [50, 40.6]
 	}
 
 # Function calculates average
@@ -47,7 +47,7 @@ def getAverage(marks):
 def calculateTotalAverage(students):
 	assignment = getAverage(students["assignment"])
 	test = getAverage(students["test"])
-	lab = getAverage(students["lab"])
+	lab = getAverage(students["class"])
 
 	# Return the result based
 	# on weightage supplied
